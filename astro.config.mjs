fb605223f66a901e61b97d875144b9fcc17d4eff
Duy Huynh <huynhtruongdyu@gmail.com>
2026-05-11 10:41:23 +0700

    feat: implement SCSS architecture and configure multi-language support in Astro project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    i18n:{
+        locales: ["en", "vi"],
+        defaultLocale: "en",
+        routing: {
+            prefixDefaultLocale: false,
+            fallbackType: "redirect"
+        }
+    }
+});
